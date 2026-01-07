bdabelow/pycamt

commit a5599e5a61f6bf97c07f9a5e568950156e8c3176

- include AccountIBAN for multi statement files in each transaction

pycamt/parser.py
@@ -145,12 +145,16 @@ class Camt053Parser:
             A list of dictionaries, each representing a transaction with its associated data.
         """
         transactions = []
-        entries = self.tree.findall(".//Ntry", self.namespaces)
-        for entry in entries:
-            transactions.extend(self._extract_transaction(entry))
+        statements = self.tree.findall(".//Stmt", self.namespaces)
+
+        for statement in statements:
+            entries = statement.findall(".//Ntry", self.namespaces)
+            for entry in entries:
+                transactions.extend(self._extract_transaction(entry, statement))
+
         return transactions
 
-    def _extract_transaction(self, entry):
+    def _extract_transaction(self, entry, statement):
         """
         Extracts data from a single transaction entry.
 
@@ -165,7 +169,7 @@ class Camt053Parser:
             A dictionary containing extracted data for the transaction.
         """
 
-        common_data = self._extract_common_entry_data(entry)
+        common_data = self._extract_common_entry_data(entry, statement)
         entry_details = entry.findall(".//NtryDtls", self.namespaces)
 
         transactions = []
@@ -209,7 +213,7 @@ class Camt053Parser:
 
         return status
 
-    def _extract_common_entry_data(self, entry):
+    def _extract_common_entry_data(self, entry, statement):
         """
         Extracts common data applicable to all transactions within an entry.
 
@@ -225,6 +229,11 @@ class Camt053Parser:
         """
         return {
             "TransactionID": entry.find(".//AcctSvcrRef", self.namespaces).text,
+            "AccountIBAN": (
+                statement.find(".//Acct//Id//IBAN", self.namespaces).text
+                if statement.find(".//Acct//Id//IBAN", self.namespaces) is not None
+                else None
+            ),
             "Amount": entry.find(".//Amt", self.namespaces).text,
             "Currency": entry.find(".//Amt", self.namespaces).attrib.get("Ccy"),
             "CreditDebitIndicator": entry.find(".//CdtDbtInd", self.namespaces).text,

commit 691ed1860453a95da125e38a039d21af613553b3

fix(parser): Restore support for `Rpt` in `get_transactions()`
This fixes a regression introduced in commit a5599e5a, which breaks support for parsing transactions in `Rpt` files such as Bank to Customer Account Reports (`BkToCstmrAcctRpt`).

pycamt/parser.py
@@ -85,6 +85,22 @@ class Camt053Parser:
                 return version
         return "unknown"
 
+    def _find_statements_or_reports(self):
+        """
+        Finds the 'Stmt' or 'Rpt' elements in the XML content.
+
+        Returns
+        -------
+        elements : list[Element]
+            The detected CAMT.053 'Stmt' or 'Rpt' elements.
+        """
+        stmts = self.tree.findall(".//Stmt", self.namespaces)
+        if len(stmts) != 0:
+            return stmts
+
+        # Maybe we have a Rpt file
+        return self.tree.findall(".//Rpt", self.namespaces)
+
     def get_group_header(self):
         """
         Extracts the group header information from the CAMT.053 file.
@@ -127,9 +143,8 @@ class Camt053Parser:
             A list of dictionaries, each representing a transaction with its associated data.
         """
         transactions = []
-        statements = self.tree.findall(".//Stmt", self.namespaces)
 
-        for statement in statements:
+        for statement in self._find_statements_or_reports():
             entries = statement.findall(".//Ntry", self.namespaces)
             for entry in entries:
                 transactions.extend(self._extract_transaction(entry, statement))
@@ -345,12 +360,8 @@ class Camt053Parser:
             - Currency: Account currency (if available)
         """
         statements = []
-        stmts = self.tree.findall(".//Stmt", self.namespaces)
-        if len(stmts) == 0:
-            # Maybe we have a Rpt file
-            stmts = self.tree.findall(".//Rpt", self.namespaces)
 
-        for stmt in stmts:
+        for stmt in self._find_statements_or_reports():
             # Extract IBAN
             iban = stmt.find(".//Acct//Id//IBAN", self.namespaces)
             iban_text = iban.text if iban is not None else None